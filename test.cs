cfe32126c40b95cab75eaa7abf2bebf7a0d09f0e
yutingwu <ding25025@gmail.com>
2024-03-04 11:53:24 +0800

    initial version

---
+++ test.cs
@@ -0,0 +1,37 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var t = new Test1();
+    }
+
+}
+
+public class TestBase
+{
+    public TestBase()
+    {
+        Console.Write("1");
+    }
+    public TestBase(string text)
+    {
+        Console.Write("2");
+        Console.Write(text);
+    }
+}
+public class Test1 : TestBase
+{
+    public Test1() : this("3")
+    {
+        Console.Write("4");
+    }
+    public Test1(string text) : base(text + "5")
+    {
+        Console.Write("6");
+        Console.Write(text);
+    }
+}
